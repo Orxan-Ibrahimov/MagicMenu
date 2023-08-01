742682e5f1cf189edbf3466da9bba7699b90f7d5
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2023-08-01 21:00:47 +0400

    LastUpdaten

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,22 +2,16 @@ const lists = document.querySelectorAll(".list");
 const indicator = document.querySelector(".indicator");
 
 function activeLink() {
-    let count = 0;
+  let count = 0;
   lists.forEach((item) => {
     item.classList.remove("active");
     this.classList.add("active");
-    
+
     if (!item.classList.contains("active")) count++;
-    else {
+    else
       indicator.style.transform = `translateX(${this.offsetWidth * count}px)`;
-      count = 0;
-      console.log(indicator.style.transform);
-      return 0;
-    }
   });
 }
-
-  
 
 lists.forEach((item) => {
   item.addEventListener("click", activeLink);
