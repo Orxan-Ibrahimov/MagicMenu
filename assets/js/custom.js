1069c7af2496ba670f2e3302713233316ff592ec
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2023-08-01 20:44:59 +0400

    UpdatedVersion

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,12 +1,24 @@
-const list = document.querySelectorAll('.list');
+const lists = document.querySelectorAll(".list");
+const indicator = document.querySelector(".indicator");
 
 function activeLink() {
-    list.forEach((item) => {
-        item.classList.remove('active');
-        this.classList.add('active');
-    });
+    let count = 0;
+  lists.forEach((item) => {
+    item.classList.remove("active");
+    this.classList.add("active");
+    
+    if (!item.classList.contains("active")) count++;
+    else {
+      indicator.style.transform = `translateX(${this.offsetWidth * count}px)`;
+      count = 0;
+      console.log(indicator.style.transform);
+      return 0;
+    }
+  });
 }
 
-list.forEach((item) => {
-    item.addEventListener('click', activeLink);
-})
+  
+
+lists.forEach((item) => {
+  item.addEventListener("click", activeLink);
+});
